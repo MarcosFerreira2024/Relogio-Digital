ebb5034852da592d207efb7d19d1a9d0455bad32
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-10-28 03:47:35 -0300

    script js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+let hours=document.querySelector('.hours')
+let minutes=document.querySelector('.minutes')
+let seconds=document.querySelector('.seconds')
+
+setInterval(() => {
+    hours.innerHTML=new Date().toString().slice(16,18)
+    minutes.innerHTML=new Date().toString().slice(19,21)
+    seconds.innerHTML=new Date().toString().slice(22,24)
+    
+}, 1000);
+
+
+
